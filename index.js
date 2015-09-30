f5fc65d38db20c4973ee0c7f9eb58f74f96272ed
Collin Brewer <collinbrewer@me.com>
2015-09-30 13:29:26 -0500

    NPM support.

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports={
+   SchemaAttributeView: require("./src/schema-attribute-view.js"),
+   SchemaEntityView: require("./src/schema-entity-view.js"),
+   SchemaPropertyView: require("./src/schema-property-view.js"),
+   SchemaRelationshipView: require("./src/schema-relationship-view.js"),
+   ValueView: require("./src/value-view.js"),
+};
