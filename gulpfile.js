98b795f302f2307b19f6c478ea18388033774fa4
Collin Brewer <collin@brewer-collective.com>
2016-07-11 09:05:35 -0500

    Now main is prebuilt

--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,7 @@ function buildJS() {
       .transform(browserifyCSS, {
          autoInject: true,
          // globals: true,
-         minify: false, 
+         minify: false,
          verbose: true
       })
       .transform(reactify);
@@ -61,23 +61,15 @@ function buildJS() {
       browserifyBundler = watchify(browserifyBundler);
       browserifyBundler.on('update', function() {
          console.log("watchify observed an update, building...");
-         buildWithBrowserifyBundler(browserifyBundler);
+         buildDemoWithBrowserifyBundler(browserifyBundler);
          console.log("done");
       });
    }
 
-   return buildWithBrowserifyBundler(browserifyBundler);
+   return buildDemoWithBrowserifyBundler(browserifyBundler);
 }
 
-function buildWithBrowserifyBundler(browserifyBundler) {
-
-   var handleError=function(e){
-      notify.onError({
-         title: "Build Error",
-         subtitle: "Browserify failed to build.",
-      })(e);
-      this.emit("end");
-   };
+function buildDemoWithBrowserifyBundler(browserifyBundler) {
 
    return browserifyBundler
       .bundle()
@@ -89,3 +81,36 @@ function buildWithBrowserifyBundler(browserifyBundler) {
 }
 
 gulp.task("default", ["build"]);
+
+var handleError=function(e){
+   notify.onError({
+      title: "Build Error",
+      subtitle: "Browserify failed to build.",
+   })(e);
+   this.emit("end");
+};
+
+gulp.task("clean-dist", shell.task([
+   "rm -rf dist"
+]));
+
+gulp.task("dist", ["clean-dist"], function(){
+
+   process.env.NODE_ENV="production";
+
+   var browserifyBundler = browserify({
+      entries: ["./index.js"],
+      debug: false,
+      package: {},
+      cache: {}
+   })
+   .transform(browserifyCSS, {
+      autoInject: true,
+      minify: true
+   })
+   .transform(reactify)
+   .bundle()
+   .on("error", handleError)
+   .pipe(source("index.js"))
+   .pipe(gulp.dest("./dist"));
+});
