32c7e234e17010f08fa223d9ecc9d3f462c0396f
Collin Brewer <collin@brewer-collective.com>
2016-07-20 09:19:02 -0500

    Now including bundled version in dist

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -3,43 +3,44 @@ var webpack = require('webpack');
 var TARGET = process.env.TARGET || null;
 
 var config = {
-  entry: {
-    index: './src/react-schema-ui.js'
-  },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath: 'dist/',
-    filename: 'react-schema-ui.js',
-    sourceMapFilename: 'react-schema-ui.sourcemap.js',
-    library: 'react-schema-ui', 
-    libraryTarget: 'umd'
-  },
-  module: {
-    loaders: [
-    { test: /\.(js|jsx)/, loader: 'babel-loader' },
-    { test: /\.css$/, loader: 'style-loader!css-loader'}
-    ]
-  },
-  plugins: [],
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  externals: {
-    'react': 'React'
-  },
+   entry: {
+      'react-schema-ui': './src/react-schema-ui.js'
+   },
+   output: {
+      path: path.join(__dirname, 'dist'),
+      publicPath: 'dist/',
+      filename: '[name].js',
+      sourceMapFilename: 'react-schema-ui.sourcemap.js',
+      library: 'react-schema-ui',
+      libraryTarget: 'umd'
+   },
+   module: {
+      loaders: [{
+         test: /\.(js|jsx)/,
+         loader: 'babel-loader'
+      }, {
+         test: /\.css$/,
+         loader: 'style-loader!css-loader'
+      }]
+   },
+   plugins: [],
+   resolve: {
+      extensions: ['', '.js', '.jsx']
+   },
+   externals: {
+      'react': 'React'
+   },
 };
 
-if (TARGET === 'minify') {
-  config.output.filename = 'react-schema-ui.min.js';
-  config.output.sourceMapFilename = 'react-schema-ui.min.js';
-  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
-    compress: {
+config.output.filename = 'react-schema-ui.min.js';
+config.output.sourceMapFilename = 'react-schema-ui.min.js';
+config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+   compress: {
       warnings: false
-    },
-   //  mangle: {
-   //    except: ['React', 'ReactMotion', 'Transition', 'Measure']
-   //  }
-  }));
-}
+   },
+   mangle: {
+      except: ['React']
+   }
+}));
 
 module.exports = config;
